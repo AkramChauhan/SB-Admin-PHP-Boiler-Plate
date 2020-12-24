c7d50fefbccd2fa24eb758a7a6852eb2a57871af
Akram Chauhan <akram.aic193@gmail.com>
2020-12-24 15:00:53 +0530

    Initial Commit with SB Admin Boiler Plat

---
+++ charts.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Dashboard - SB Admin</title>
+        <?php include "includes/load_css.php"; ?>
+   </head>
+    <body class="sb-nav-fixed">
+      <?php include "includes/header.php"; ?>
+        <main>
+            <?php include "pages/charts.php"; ?>
+        </main>
+      <?php include "includes/footer.php"; ?> 
+    </body>
+</html>
